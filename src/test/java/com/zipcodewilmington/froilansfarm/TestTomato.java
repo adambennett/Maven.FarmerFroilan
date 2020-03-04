ff6ed0bb548a6b75451826944c9a871f49632947
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-04 16:08:40 -0500

    add tests

--- src/test/java/com/zipcodewilmington/froilansfarm/TestTomato.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TestTomato.java
@@ -1,4 +1,18 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestTomato {
+
+    @Test
+    public void testImplementation(){
+
+        Tomato tomato = new Tomato();
+        boolean actual = tomato instanceof Edible;
+        Assert.assertTrue(actual);
+
+    }
+
 }
